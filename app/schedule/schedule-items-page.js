6b05977e9a3361d6028149b9d92373c12e05cad2
webnicola <webnicola@gmail.com>
2019-11-25 17:33:24 +0100

    remove onItemTap ListView

--- app/schedule/schedule-items-page.js
+++ app/schedule/schedule-items-page.js
@@ -5,22 +5,4 @@ function onNavigatingTo(args) {
     component.bindingContext = new ScheduleItemsViewModel();
 }
 
-function onItemTap(args) {
-    const view = args.view;
-    const page = view.page;
-    const tappedItem = view.bindingContext;
-
-    page.frame.navigate({
-        moduleName: "schedule/schedule-item-detail/schedule-item-detail-page",
-        context: tappedItem,
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 200,
-            curve: "ease"
-        }
-    });
-}
-
-exports.onItemTap = onItemTap;
 exports.onNavigatingTo = onNavigatingTo;
