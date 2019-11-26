ab916bafe4a8c7cfee2922655515474d3effc7cc
webnicola <webnicola@gmail.com>
2019-11-26 16:39:16 +0100

    data, style

--- app/schedule/schedule-items-view-model.js
+++ app/schedule/schedule-items-view-model.js
@@ -1,73 +1,13 @@
 const observableModule = require("tns-core-modules/data/observable");
+const data = require('./data')
+const redata = data.map((item) => {
+    item.sub = `${item.inizio} — ${item.fine} ${item.luogo}`
+    return item
+});
 
 function ScheduleItemsViewModel() {
     const viewModel = observableModule.fromObject({
-        items: [
-            {
-                name: "Schedule 1",
-                description: "Schedule for Item 1"
-            },
-            {
-                name: "Schedule 2",
-                description: "Schedule for Item 2"
-            },
-            {
-                name: "Schedule 3",
-                description: "Schedule for Item 3"
-            },
-            {
-                name: "Schedule 4",
-                description: "Schedule for Item 4"
-            },
-            {
-                name: "Item 5",
-                description: "Description for Item 5"
-            },
-            {
-                name: "Item 6",
-                description: "Description for Item 6"
-            },
-            {
-                name: "Item 7",
-                description: "Description for Item 7"
-            },
-            {
-                name: "Item 8",
-                description: "Description for Item 8"
-            },
-            {
-                name: "Item 9",
-                description: "Description for Item 9"
-            },
-            {
-                name: "Item 10",
-                description: "Description for Item 10"
-            },
-            {
-                name: "Item 11",
-                description: "Description for Item 11"
-            },
-            {
-                name: "Item 12",
-                description: "Description for Item 12"
-            },
-            {
-                name: "Item 13",
-                description: "Description for Item 13"
-            },
-            {
-                name: "Item 14",
-                description: "Description for Item 14"
-            },
-            {
-                name: "Item 15",
-                description: "Description for Item 15"
-            },
-            {
-                name: "Item 16",
-                description: "Description for Item 16"
-            }
-        ]
+        items: redata
     });
 
     return viewModel;
