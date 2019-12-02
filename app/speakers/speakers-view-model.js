c2b4c6226aa8c5eca4ce26190ca24cc3a118de50
webnicola <webnicola@gmail.com>
2019-12-02 17:03:47 +0100

    speakers by data

--- app/speakers/speakers-view-model.js
+++ app/speakers/speakers-view-model.js
@@ -1,11 +1,13 @@
 const observableModule = require("tns-core-modules/data/observable");
-
+const data = require('./data');
+const redata = data.map((item) => {
+    item.interventocompleto = `${item.nome} — ${item.inizio} ${item.fine}`
+    return item
+});
 function SpeakerViewModel() {
     const viewModel = observableModule.fromObject({
-        /* Add your view model properties here */
-        test: 'ciao mondo'
+       items: redata
     });
-
     return viewModel;
 }
 
