6930b1403def283973d3093c46c88a01b23f7e6b
webnicola <webnicola@gmail.com>
2019-12-04 17:13:36 +0100

    add markers form geojson data

--- app/map/map-page.js
+++ app/map/map-page.js
@@ -1,33 +1,52 @@
 const MapViewModel = require("./map-view-model");
 const mapsModule = require("nativescript-google-maps-sdk");
-
+const geoJson = require("./data");
 function onNavigatingTo(args) {
     const component = args.object;
     component.bindingContext = new MapViewModel();
 }
 
-function onMapReady(args) {
-    console.log("onMapReady", args);
-    const mapView = args.object;
-    const marker = new mapsModule.Marker();
-    marker.position = mapsModule.Position.positionFromLatLng(
-        45.649268385921395,
-        13.762092590332031
-    );
-    marker.title = "Colazione";
-    marker.color = "green";
-    marker.userData = {index: 1};
-    mapView.addMarker(marker);
+function isValidGEOJSON(obj) {
+    try {
+        if (obj.hasOwnProperty('features') && obj.features.length > 0)
+            return true
+        else
+        console.error('il tuo geoJson non valido')
+    } catch {
+      return false;
+    }
+}
 
-    const marker2 = new mapsModule.Marker();
-    marker2.position = mapsModule.Position.positionFromLatLng(
-        45.646283314737836,
-        13.759925365447998
-    );
-    marker2.title = "Colazione";
-    marker2.color = "green";
-    marker2.userData = {index: 1};
-    mapView.addMarker(marker2);
+function onMapReady(args) {
+    const mapView = args.object;
+
+    if (isValidGEOJSON(geoJson)) {
+        geoJson.features.forEach(feature => {
+            console.log('feature', feature.geometry.coordinates[0]);
+            const marker = new mapsModule.Marker();
+            marker.position = mapsModule.Position.positionFromLatLng(
+                feature.geometry.coordinates[1],
+                feature.geometry.coordinates[0]
+            );
+            marker.title = feature.properties.evento;
+            marker.color = "blue";
+            marker.userData = {index: 1};
+            mapView.addMarker(marker);
+        });
+    }
+
+
+
+
+    // const marker = new mapsModule.Marker();
+    // marker.position = mapsModule.Position.positionFromLatLng(
+    //     45.649268385921395,
+    //     13.762092590332031
+    // );
+    // marker.title = "Colazione";
+    // marker.color = "green";
+    // marker.userData = {index: 1};
+    // mapView.addMarker(marker);
 
 }
 
