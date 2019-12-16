986a51b8367ccc863348f98f81d6b58da35933d0
webnicola <webnicola@gmail.com>
2019-12-16 15:22:42 +0100

    add nafigation function

--- app/information/information-page.js
+++ app/information/information-page.js
@@ -5,4 +5,13 @@ function onNavigatingTo(args) {
     component.bindingContext = new SearchViewModel();
 }
 
+function onTap(args) {
+    const button = args.object;
+    const page = button.page;
+    page.frame.navigate("map/map-page");
+    // alert('FIRE!!!!');
+}
+
+
 exports.onNavigatingTo = onNavigatingTo;
+exports.onTap = onTap;
