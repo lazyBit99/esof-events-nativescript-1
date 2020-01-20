3fc1393f5667fd080030d7aed38fc34f5f4b30cb
webnicola <webnicola@gmail.com>
2020-01-20 15:10:00 +0100

    add authentication

--- app/app.js
+++ app/app.js
@@ -1,14 +1,32 @@
 const application = require("tns-core-modules/application");
 const config = require("./config");
+const firebase = require("nativescript-plugin-firebase");
 const MapAPI = config.google.map.provideAPIKey || "AIzaSyCPsksKb67hdxuVZJXyzcsPwcRHPzFyrJ4";
 // Google Maps SDK API KEY
 if(application.ios) {
     GMSServices.provideAPIKey(MapAPI);
 }
 
+firebase.init({
+    iOSEmulatorFlush: true,
+    persist: false,
+    onAuthStateChanged: function(data) {
+        console.log(data.loggedIn ? 'Logged' : 'Logged out')
+        if (data.loggedIn) {
+            console.log(' INIT USER', data)
+        }
+    }
+}).then(
+    function () {
+        console.log("Firebase.init done");
+    },
+    function (error) {
+        console.log("firebase.init error: " + error);
+    }
+);
+
 
 application.run({ moduleName: "app-root" });
-
 /*
 Do not place any code after the application has been started as it will not
 be executed on iOS.
--- app/schedule/schedule-items-page.js
+++ app/schedule/schedule-items-page.js
@@ -1,4 +1,35 @@
+const firebase = require("nativescript-plugin-firebase");
 const ScheduleItemsViewModel = require("./schedule-items-view-model");
+
+// firebase.login({
+//     type: firebase.LoginType.PASSWORD,
+//     passwordOptions: {
+//         email: 'webnicola@gmail.com',
+//         password: 'theirpassword'
+//     }
+// })
+// .then(result => {
+//     console.log('OK', result)
+//     alert({
+//         title: 'Autetenticazione OK',
+//         message: `Ciao, ${result.email}`,
+//         okButtonText: 'OK'
+//     })
+// })
+// .catch(error => console.error(error))
+
+// firebase.logout()
+// .then(() => console.log('LOGOUT'))
+// .catch(error => console.error(error))
+
+// firebase.sendPasswordResetEmail('webnicola@gmail.com')
+// .then(() => console.log('Password reset by email'))
+// .catch(error => console.error('error', error))
+
+firebase.getCurrentUser()
+.then(user => console.log('**user**', user))
+.catch(error => console.error('No user find', error))
+
 
 function onNavigatingTo(args) {
     const component = args.object;
